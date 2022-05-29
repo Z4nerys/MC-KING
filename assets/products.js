ce4f50c131f2b9ad78059f72b56dbe139abb1c16
Gasty <gmfkratze@gmail.com>
2022-05-29 20:17:50 -0300

    products data added

---
+++ assets/products.js
@@ -0,0 +1,200 @@
+export const products = [
+    {
+        "id" : "Hamburguesa Big Mc",
+        "nombre" : "Big MAC",
+        "ingredientes" : "Hamburguesa 100% carne con salsa especial Bigmac, cebolla, lechuga, pepinillo y queso",
+        "descripción" : "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "calorias" : "505 kcal",
+        "precio" : "$ 590"
+        
+    },
+    {
+        "id" : "Helado combinado",
+        "nombre" : "Cono Combinado",
+        "ingredientes" : "Crema helada combinada con sabor vainilla y dulce de leche",
+        "descripción" : "El helado para los que lo quieren todo. Para los que no se conforman con un sabor. Un helado que combina dulce de leche, vainilla y un delicioso cucurucho.",
+        "calorias" : "184 kcal",
+        "precio" : "$ 100"
+        
+    },
+    {
+        "id" : "Helado dulce de leche",
+        "nombre" : "Cono de Dulce de Leche",
+        "ingredientes" : "En este establecimiento se elaboran alimentos que contienen los siguientes alérgenos: Mostaza, Trigo, Avena, Cebada, Centeno, Huevo, Leche, Soja, Sésamo, Pescado, Sulfito > a 10 ppm, Pimienta, Frutos secos, Maní",
+        "descripción" : "Si amás el dulce de leche, si amás el helado, si amás el cucurucho, si amás un postre que te refresque; este es tu postre.",
+        "calorias" : "193 kcal",
+        "precio" : "$ 100"
+        
+    },
+    {
+        "id" : "Helado vainilla",
+        "nombre" : "Cono de Vainilla",
+        "ingresientes" : "En este establecimiento se elaboran alimentos que contienen los siguientes alérgenos: Mostaza, Trigo, Avena, Cebada, Centeno, Huevo, Leche, Soja, Sésamo, Pescado, Sulfito > a 10 ppm, Pimienta, Frutos secos, Maní",
+        "descripción" : "Un clásico cono que nunca pasa de moda. La opción que todos aman por su sabor, su dulce cucurucho y su estilo tan cool.",
+        "calorias" : "175 kcal",
+        "precio" : "$ 100"
+        
+    },
+    {
+        "id" : "Helado kitkat",
+        "nombre" : "Cono Kit Kat",
+        "ingredientes" : "En este establecimiento se elaboran alimentos que contienen los siguientes alérgenos: Mostaza, Trigo, Avena, Cebada, Centeno, Huevo, Leche, Soja, Sésamo, Pescado, Sulfito > a 10 ppm, Pimienta, Frutos secos, Maní",
+        "descripción" : "Cremoso helado de vainilla, en un delicioso cucurucho, acompañado de la crujiente oblea de chocolate, la favorita de todos: Kit Kat. ¡Imposible resistirse!",
+        "calorias" : "190 Kcal",
+        "precio" : "$ 140"
+        
+    },
+    {
+        "id" : "Helado relleno",
+        "nombre" : "Cono Relleno",
+        "ingredientes" : "En este establecimiento se elaboran alimentos que contienen los siguientes alérgenos: Mostaza, Trigo, Avena, Cebada, Centeno, Huevo, Leche, Soja, Sésamo, Pescado, Sulfito > a 10 ppm, Pimienta, Frutos secos, Maní",
+        "descripción" : "Helado de vainilla, dulce de leche o combinado, servido en cono de masa crocante y con relleno de chocolate, frutilla o dulce de leche. ¿Cuál elegis?",
+        "calorias" : "225 Kcal",
+        "precio" : "$ 130"
+        
+    },
+    {
+        "id" : "Hamburguesa cuarto de libra con queso",
+        "nombre" : "Cuarto de Libra con Queso",
+        "ingredientes" : "Hamburguesa de carne 100% vacuna, dos fetas de queso cheddar, kétchup, mostaza y cebolla fresca. Ahora podes pedirlo con y sin pepinos.",
+        "descripción" : "La belleza radica en la simpleza de las cosas. Una hamburguesa que no se anda con vueltas. La perfecta combinación entre la mejor carne 100% vacuna y dos quesos que lo rodean, junto con el toque del kétchup, mostaza y la cebolla fresca.",
+        "calorias" : "520 kcal",
+        "precio" : "$ 590"
+        
+    },
+    {
+        "id" : "Hamburguesa doble cuarto de libra con queso",
+        "nombre" : "Doble Cuarto de Libra con Queso",
+        "ingredientes" : "Hamburguesa con doble carne 100% vacuna, dos fetas de queso cheddar, kétchup, mostaza y cebolla fresca. Ahora podes pedirlo con y sin pepinos.",
+        "descripción" : "Imaginá la sensación del clásico Cuarto de Libra. Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza.",
+        "calorias" : "771 kcal",
+        "precio" : "$ 680"
+        
+    },
+    {
+        "id" : "Ensalada huerta",
+        "nombre" : "Ensalada de la huerta",
+        "ingredientes" : "Ensalada con ingredientes tradicionales: tomate cherry, lechuga, pepino una exquisita combinación entre los vegetales, texturas crocantes y dressing",
+        "descripción" : "¡Los productos más frescos para nuestra ensalada más fresca! De lo mejor de nuestra huerta sale nuestra mejor ensalada, una base de ensalada acompañada con tomates cherry. Un complemento ideal para acompañar tu McMenú®. Nuestras ensaladas se elaboran diariamente con productos frescos de Florette®.",
+        "calorias" : "31 Kcal",
+        "precio" : "$ 360"
+        
+    },
+    {
+        "id" : "Ensalada de queso cabra",
+        "nombre" : "Ensalada queso de cabra",
+        "ingredientes" : "Ensalada con ingredientes de calidad: tomate cherry, lechuga, mix verdes, pepino, queso cabra y crutones una exquisita combinación entre los vegetales, texturas crocantes y dressing",
+        "descripción" : "¡Saborea nuestra nueva ensalada de queso de cabra con mix de ensalada, cherritos y croutons! Nuestras ensaladas se elaboran diariamente con productos frescos de Florette®",
+        "calorias" : "68 Kcal",
+        "precio" : "$ 480"
+        
+    },
+    {
+        "id" : "Cafe expresso",
+        "nombre" : "Espresso",
+        "ingredientes" : "Cafe expresso",
+        "descripción" : "La mejor selección de finos granos de café arábicos en todo su esplendor, para que disfrutes sorbo a sorbo.",
+        "calorias" : "25 Kcal",
+        "precio" : "$ 150"
+        
+    },
+    {
+        "id" : "Cafe expresso",
+        "nombre" : "Espresso",
+        "ingredientes" : "Cafe expresso",
+        "descripción" : "La mejor selección de finos granos de café arábicos en todo su esplendor, para que disfrutes sorbo a sorbo.",
+        "calorias" : "25 Kcal",
+        "precio" : "$ 150"
+        
+    },
+    {
+        "id" : "Frappé de frutilla con banana",
+        "nombre" : "Frappé de Frutilla con Banana",
+        "ingredientes" : "Bebida fresca combinada con un mix de frutilla, banana y helado sabor vainilla",
+        "descripción" : "Probá tus frutas favoritas dentro de un mismo batido fresco. Acá está la bebida ideal que buscabas para ganarle al calor.",
+        "calorias" : "387.78 Kcal",
+        "precio" : "$ 350"
+        
+    },
+    {
+        "id" : "Frappé clásico",
+        "nombre" : "Frappé",
+        "ingredientes" : "Bebida fresca hecha con cafe, acompañado con un suave colchon de crema y salsa de chocolate",
+        "descripción" : "El mismo café que te ayuda a levantarte todos los días, ahora con una sensación refrescante, acompañado de crema e hilos de chocolate.",
+        "calorias" : "146 Kcal",
+        "precio" : "$ 310"
+        
+    },
+    {
+        "id" : "Hamburguesa Grand doble McBacon",
+        "nombre" : "Grand Doble McBacon",
+        "ingredientes" : "Hamburguesa con doble carne 100% vacuna, bacon premium, queso cheddar, cebolla, kétchup y mostaza en un pan más grande con semillas",
+        "descripción" : "¡NUEVO PAN! Dos carnes, inigualable queso cheddar, cebolla, kétchup y mostaza, y el increíble ingrediente que lo hace único: bacon premium.",
+        "calorias" : "942 Kcal",
+        "precio" : "$ 680"
+        
+    },
+    {
+        "id" : "Hamburguesa GRAND TASTY TRIPLE",
+        "nombre" : "Grand Tasty Triple",
+        "ingredientes" : "Hamburguesa con triple carne 100% vacuna, salsa tasty, queso cheddar, lechuga, tomate, cebolla en un pan más grande con semillas",
+        "descripción" : "Inigualable pan con semillas de sésamo, tres medallones de carne 100% vacuna, cuatro fetas de nuestro exclusivo Queso Cheddar, cebolla, lechuga y tomate frescos. Sumado a estos ingredientes la única e inigualable Salsa Tasty, un exclusivo sabor McDonald’s",
+        "calorias" : "1153 Kcal",
+        "precio" : "$ 840"
+        
+    },
+    {
+        "id" : "Hamburguesa Grand triple McBacon",
+        "nombre" : "Grand Triple McBacon",
+        "ingredientes" : "Hamburguesa con triple carne 100% vacuna, bacon premium, queso cheddar, cebolla, kétchup y mostaza en un pan más grande con semillas",
+        "descripción" : "¡NUEVO PAN! Triple carne, inigualable queso cheddar, cebolla, kétchup y mostaza, y el increíble ingrediente que lo hace único: bacon premium",
+        "calorias" : "1233 Kcal",
+        "precio" : "$ 840"
+        
+    },
+    {
+        "id" : "Hamburguesa con queso",
+        "nombre" : "Hamburguesa con Queso",
+        "ingredientes" : "Hamburguesa 100% vacuna con queso cheddar, cebolla, mostaza y ketchup",
+        "descripción" : "Carne 100% vacuna, queso derretido y mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar. Un clásico que nunca pasa de moda.",
+        "calorias" : "289 kcal",
+        "precio" : "$ 400"
+        
+    },
+    {
+        "id" : "Hamburguesa clásica",
+        "nombre" : "Hamburguesa",
+        "ingredientes" : "Hamburguesa 100% vacuna, cebolla, mostaza y ketchup",
+        "descripción" : "El sabor de la carne 100% vacuna más deliciosa, acompañado del pan más esponjoso, kétchup, mostaza y cebolla triturada.",
+        "calorias" : "243 kcal",
+        "precio" : "$ 350"
+        
+    },
+    {
+        "id" : "Combo cajita",
+        "nombre" : "Cajita Feliz",
+        "ingredientes" : "Hamburgusa 100% vacuna, papas fritas kids, bebida, YogurKid.",
+        "descripción" : "El sabor de la carne 100% vacuna más deliciosa, acompañado con las clásicas papas fritas crocantes realizadas en el momento, bebida fresca y un juguete ¡Elegí entre Libro o Juguete!",
+        "calorias" : "228.8 Kcal",
+        "precio" : "$ 750"
+        
+    },
+    {
+        "id" : "Cafe Latte",
+        "nombre" : "Latte",
+        "ingredientes" : "40% café americano y 60% de leche",
+        "descripción" : "Delicioso espresso con vainilla, leche con espuma y por si fuera poco, acompañado de un increíble crocante de caramelo.",
+        "calorias" : "40 Kcal",
+        "precio" : "$ 180"
+        
+    },
+    {
+        "id" : "",
+        "nombre" : "",
+        "ingredientes" : "",
+        "descripción" : "",
+        "calorias" : "",
+        "precio" : "$ "
+        
+    }
+]
